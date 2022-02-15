eaca0257dcf874f0756e4a126aabcda829baa5f9
moin21 <mamoinuddin@gmail.com>
2022-02-15 16:03:25 +0530

    Added: Current position is start

--- src/com/snakeandladderprogram/SnakeAndLadderProgram.java
+++ src/com/snakeandladderprogram/SnakeAndLadderProgram.java
@@ -1,9 +1,13 @@
 package com.snakeandladderprogram;
 
 public class SnakeAndLadderProgram {
+	public static final int START_POSITION = 0;
+	public static final int FINAL_POSITION = 100;
+	public static int currentPosition;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game!!");
+		currentPosition = START_POSITION;
 	}
 
 }
