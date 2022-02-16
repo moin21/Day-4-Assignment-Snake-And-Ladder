47f69fd27fb8a929726ee5441ffebe8a35e9c3b4
moin21 <mamoinuddin@gmail.com>
2022-02-16 22:53:01 +0530

    Added: Logic for 2 players

--- src/com/snakeandladderprogram/SnakeAndLadderProgram.java
+++ src/com/snakeandladderprogram/SnakeAndLadderProgram.java
@@ -3,16 +3,23 @@ package com.snakeandladderprogram;
 public class SnakeAndLadderProgram {
 	public static final int START_POSITION = 0;
 	public static final int FINAL_POSITION = 100;
-	public static int currentPosition;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game!!");
-		currentPosition = START_POSITION;
-		diceRoll();
+		int rollPlayer1 = diceRoll("player1");
+		int rollPlayer2 = diceRoll("player2");
+		if (rollPlayer1 > rollPlayer2) {
+			System.out.println("Player2 is the winner");
+		} else if (rollPlayer1 < rollPlayer2) {
+			System.out.println("Player1 is the winner");
+		} else {
+			System.out.println("It is a tie.");
+		}
 
 	}
 
-	public static void diceRoll() {
+	public static int diceRoll(String player) {
+		int currentPosition = START_POSITION;
 		int noOfRolls = 0;
 		while (currentPosition < FINAL_POSITION) {
 			int diceResult = (int) (Math.floor(Math.random() * 10) % 6 + 1);
@@ -33,6 +40,7 @@ public class SnakeAndLadderProgram {
 			}
 			System.out.println(currentPosition);
 		}
-		System.out.println("No. of total Dice Rolls are: "+noOfRolls);
+		System.out.println("No. of total Dice Rolls by " + player + " are: " + noOfRolls);
+		return noOfRolls;
 	}
 }
